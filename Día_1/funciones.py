3ca6285ef8eb3e36f10a680b6a6488cceaef3107
Daniela <dani.didi.fb@gmail.com>
2024-01-25 13:04:21 -0500

    Suma check

--- Día_1/funciones.py
+++ Día_1/funciones.py
@@ -1,7 +1,9 @@
-num1=input("Ingrese un número: ")
-num2=input("Ingrese otro número: ")
+
+
+num1=int(input("Ingrese un número: "))
+num2=int(input("Ingrese otro número: "))
 def suma(num1,num2):
     ##Esta función genera la suma de 2 números ingresados por el usuario :D
     rta=num1+num2
-    return
+    return rta
 print("La suma de los dos números ingresados es: ",suma(num1,num2))
