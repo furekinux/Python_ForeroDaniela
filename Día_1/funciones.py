5c546ad690e9b2834a76438699930a219b252ecc
Daniela <dani.didi.fb@gmail.com>
2024-01-25 12:57:06 -0500

    funciones intento ;_;

---
+++ Día_1/funciones.py
@@ -0,0 +1,7 @@
+num1=input("Ingrese un número: ")
+num2=input("Ingrese otro número: ")
+def suma(num1,num2):
+    ##Esta función genera la suma de 2 números ingresados por el usuario :D
+    rta=num1+num2
+    return
+print("La suma de los dos números ingresados es: ",suma(num1,num2))
