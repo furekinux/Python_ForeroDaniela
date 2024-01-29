ef900f3654ed82ba89121bd2774a1ec809288b06
Daniela Forero Ballén <156367697+furekinux@users.noreply.github.com>
2024-01-29 05:56:02 -0500

    Create Ejercicio1_fibonacci_ForeroDaniela.py
    bouken do bouken

---
+++ Día_2/Ejercicio1_fibonacci_ForeroDaniela.py
@@ -0,0 +1,30 @@
+##Fibonacci
+
+a=0##Valotes
+b=1##Iniciales
+d=1
+
+def genfibo():
+    while n>d+1: ##Mientras d es menor que c repetir:
+        a+b==a
+        b+a==b
+    print(a)
+    print(b)
+    d=d+1
+    return Fibonacci
+
+print("Bienvenido usuario, por medio de este programa podrá generar la secuencia de fibonnaci")
+rta=str(input("¿Empezar?"))
+if rta=="Si":
+  n=int(input("Ingrese la cantidad de pasos a generar: ")) ##El usuario ingresa la cantidad de numeros a generar
+  print(genfibo())
+  print("¿Desea agregar más digitos?")
+  rta=str(input("RTA: "))
+  if rta=="Si":
+      n=int(input("Ingrese la cantidad de pasos a generar: ")) ##El usuario ingresa la cantidad de numeros a generar
+      print(genfibo())
+  else:
+      print("Fin del proceso")
+
+else:
+  print("Fin del proceso")
