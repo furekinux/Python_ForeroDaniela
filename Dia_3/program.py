c7715f61ee8602f054a69ee48d61fdc3e975aa42
Daniela Forero Ballén <dani.didi.fb@gmail.com>
2024-01-29 13:00:14 -0500

    ansiedad

--- Dia_3/program.py
+++ Dia_3/program.py
@@ -1,14 +1,7 @@
-a1=1
-b5=5
-c10=10
+n=(int(input()))
 
 def coins():
-    ng=[]
-    while (n>0):
-        ng.append(1)
-    print(f"{n}={ng}")
-
-
-n=(input())
+    a=[(int(n/10)),(int((n%10)/5)),(int(((n%10)%5)/1))]
+    print(a)
 
 coins()
