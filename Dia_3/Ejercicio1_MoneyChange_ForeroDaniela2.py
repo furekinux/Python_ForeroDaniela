d903d672461823ddc0ac7ca0993defb64f748ec6
Daniela Forero Ballén <dani.didi.fb@gmail.com>
2024-01-30 06:28:28 -0500

    EEEEEEE

---
+++ Dia_3/Ejercicio1_MoneyChange_ForeroDaniela2.py
@@ -0,0 +1,8 @@
+##AAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHH
+n=(int(input()))
+def coins():
+    if n>=1 and n<=1000:
+        print(f"{(int(n/10))+(int((n%10)/5))+(int(((n%10)%5)/1))}")
+    else:
+        print("ERROR")
+coins()
