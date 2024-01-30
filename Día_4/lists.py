6a00051bacf30ffcc7bbda1407d63d54353bd5e3
Daniela Forero Ballén <dani.didi.fb@gmail.com>
2024-01-30 14:04:44 -0500

    AAAA

--- Día_4/lists.py
+++ Día_4/lists.py
@@ -1,4 +1,20 @@
-for aAAAAAAAAAA in 4:
-    a+b
-    
+def nkCheck():
+    if not 1<n<100001:
+        exit()
+    if not 0<k<501:
+        exit()
+
+T=(int(input()))
+if 0>T>100:
+     exit()
+else:
+    nk=input("")
+    a=[*nk]
+    for i in range(2):
+        data=int(a[i])
+        a[i]=data
+    print(a)
+    n=2
+    k=3
+    nkCheck
 
