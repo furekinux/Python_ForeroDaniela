7f772bc41a8e75365648ec5f60669e11ecfc4336
Daniela Forero Ballén <dani.didi.fb@gmail.com>
2024-01-30 14:44:40 -0500

    tuki

--- Día_4/lists.py
+++ Día_4/lists.py
@@ -4,6 +4,13 @@ def nkCheck():
     if not 0<k<501:
         exit()
 
+from os import system,name
+def clear():
+    if name=="nt":
+        _=system("cls")
+    else:
+        _=system("clear")
+
 T=(int(input()))
 if 0>T>100:
      exit()
@@ -13,8 +20,16 @@ else:
     for i in range(2):
         data=int(a[i])
         a[i]=data
-    print(a)
-    n=2
-    k=3
+        i=i+1
+        n=int(a[0])
+        k=int(a[1])
     nkCheck
-
+    print(f"{T}\n{n} {k}")
+    aaa=input("")
+    b=[*aaa]
+    for l in range(n):
+        dataA=int(b[l])
+        b[l]=dataA
+        l=l+1
+    print(b)
+    
