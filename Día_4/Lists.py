8f0437f94aa69597215715c4e73974b6e68c192d
Daniela Forero Ballén <dani.didi.fb@gmail.com>
2024-01-31 08:38:36 -0500

    ;_;;;;

--- Día_4/Lists.py
+++ Día_4/Lists.py
@@ -1,4 +1,25 @@
+def pares(T_n,n,k):
+    pairs=set()
+    for i in range(n):
+        for j in range(i+1,n):
+            if(T_n[i]+T_n[j])%k==0:
+                pairs.add((min(T_n[i],T_n[j])))
+    return len(pairs)
+
+
+
 T=int(input())
+for case in range(T):
+    n,k=map(int,input().split())
+    T_n=list(map(int,input().split()))
+    print(T_n)
+    print(n)
+    print(k)
+    result=pares(T_n,n,k)
+    print("Case {}:{}".format(case+1,result))
+
+exit()
+
 for case in range(T):
     text=input("")
     nums=input("")
@@ -12,5 +33,6 @@ for case in range(T):
     T_n[p]=nums.split
     for p in range(n):
         number=int(numsLista[p])
-        T_n[p]=number
-    result=pares
+        T_n.append(number)
+    result=pares(T_n,n,k)
+    print("Case {}:{}".format(case+1,result))
