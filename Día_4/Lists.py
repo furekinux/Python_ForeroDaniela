97352eff45bd244bb8cf853589b342dc89d3ad59
Daniela Forero Ballén <dani.didi.fb@gmail.com>
2024-01-31 13:06:13 -0500

    Holiwis

--- Día_4/Lists.py
+++ Día_4/Lists.py
@@ -1,9 +1,9 @@
-def pares(T_n,n,k):
+def check(Interg,n,k):
     pairs=set()
     for i in range(n):
         for j in range(i+1,n):
-            if(T_n[i]+T_n[j])%k==0:
-                pairs.add((min(T_n[i],T_n[j]),max(T_n[i],T_n[j])))
+            if(Interg[i]+Interg[j])%k==0:
+                pairs.add((min(Interg[i],Interg[j]),max(Interg[i],Interg[j])))
     return len(pairs)
 
 T=int(input())
@@ -12,45 +12,13 @@ for case in range(T):
     nums=input("")
     n=0
     k=0
-    T_n=list()
+    Interg=list()
     textoLista=text.split(" ")
     numsLista=nums.split(" ")
     n=int(textoLista[0])
     k=int(textoLista[1])
     for p in range(n):
         number=int(numsLista[p])
-        T_n.append(abs(number))
-    result=pares(T_n,n,k)
-    print("Case {}:{}".format(case+1,result))
-
-
-exit()
-
-T=int(input())
-for case in range(T):
-    n,k=map(int,input().split())
-    T_n=list(map(int,input().split()))
-    print(T_n)
-    print(n)
-    print(k)
-    result=pares(T_n,n,k)
-    print("Case {}:{}".format(case+1,result))
-
-
-
-for case in range(T):
-    text=input("")
-    nums=input("")
-    n=0
-    k=0
-    T_n=list()
-    textoLista=text.split(" ")
-    numsLista=nums.split(" ")
-    n=int(textoLista[0])
-    k=int(textoLista[1])
-    T_n[p]=nums.split
-    for p in range(n):
-        number=int(numsLista[p])
-        T_n.append(number)
-    result=pares(T_n,n,k)
+        Interg.append(abs(number))
+    result=check(Interg,n,k)
     print("Case {}:{}".format(case+1,result))
