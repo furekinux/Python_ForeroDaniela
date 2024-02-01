cb240364fc2ec10286fc8c68baf0f4363537063a
Daniela Forero Ballén <dani.didi.fb@gmail.com>
2024-02-01 07:47:53 -0500

    Studying

---
+++ Dia_6/diccio.py
@@ -0,0 +1,11 @@
+lista={'Nombre':'Ella','Edad':20,'Barrio':'Zapamanga'}
+print(lista)
+print(type(lista))
+print(lista['Nombre'])
+lista['Nombre']="Jhon"##CAMBIA NOMBRE O ITEM
+print(lista)
+print(type(lista))
+print(lista['Nombre'])
+
+for i in lista:
+    print(i)
--- Día_5/EJ2_Coins__Forero&Mulford.py
+++ Día_5/EJ2_Coins__Forero&Mulford.py
@@ -1,17 +1,20 @@
-def ntcheck():
-    if 4>=n>=50:
-        exit()
+def ntcheck(n,t):
+    if 4<=n<=50:
+        if 1<=t<=10:
+           pass
+        else:
+           return exit()
     else:
-        pass
-    if 1>=t>=10:
-       exit()
-    else:
-        pass
-
-
+        return exit()
+    
 
 nt=(input())
 ntList=nt.split(" ")
-n=int(ntList[0])
-t=int(ntList[1])
-print(n,t)
+n=int(ntList[0]) ##Types of coins
+t=int(ntList[1]) ##Number of tables
+ntcheck
+LList=list()
+for i in range(n):
+    L=input("")
+    LList.append(L)
+print(LList)
