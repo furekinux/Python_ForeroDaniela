6738c42a319a2053c23b2db9057c49ebb64b88a9
Daniela Forero Ballén <dani.didi.fb@gmail.com>
2024-02-01 07:26:51 -0500

    Bouken do bouken

---
+++ Día_5/EJ2_Coins__Forero&Mulford.py
@@ -0,0 +1,17 @@
+def ntcheck():
+    if 4>=n>=50:
+        exit()
+    else:
+        pass
+    if 1>=t>=10:
+       exit()
+    else:
+        pass
+
+
+
+nt=(input())
+ntList=nt.split(" ")
+n=int(ntList[0])
+t=int(ntList[1])
+print(n,t)
