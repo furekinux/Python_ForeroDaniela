a75747169e901bd927490cf2c2846b113e2df78c
Daniela Forero Ballén <dani.didi.fb@gmail.com>
2024-02-02 08:35:48 -0500

    Wiiiiiiiiii

--- Día_2/Ejercicio1_fibonacci_ForeroDaniela.py
+++ Día_2/Ejercicio1_fibonacci_ForeroDaniela.py
@@ -4,18 +4,17 @@ a=0##Valotes
 b=1##Iniciales
 d=1
 
-XD.genfibo()
 
 print("Bienvenido usuario, por medio de este programa podrá generar la secuencia de fibonnaci")
 rta=str(input("¿Empezar?"))
 if rta=="Si":
   n=int(input("Ingrese la cantidad de pasos a generar: ")) ##El usuario ingresa la cantidad de numeros a generar
-  genfibo()
+  XD.genfibo(a,b,n,d)
   print("¿Desea agregar más digitos?")
   rta=str(input("RTA: "))
   if rta=="Si":
       n=int(input("Ingrese la cantidad de pasos a generar: ")) ##El usuario ingresa la cantidad de numeros a generar
-      print(genfibo(a,b,n,d))
+      print(XD.genfibo(a,b,n,d))
   else:
       print("Fin del proceso")
 
