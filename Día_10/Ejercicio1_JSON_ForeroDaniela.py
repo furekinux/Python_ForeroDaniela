61cbf66b83070a10d248de890af9ebf7c6b78cb8
Daniela Forero Ballen <dani.didi.fb@gmail.com>
2024-02-07 14:51:51 -0500

    help

--- Día_10/Ejercicio1_JSON_ForeroDaniela.py
+++ Día_10/Ejercicio1_JSON_ForeroDaniela.py
@@ -4,12 +4,13 @@ import json
 ##2. Devuelve todos los datos de los dos pedidos de mayor valor.
 ##3. Devuelve un listado con los identificadores de los clientes que han realizado algún pedido. Tenga en cuenta que no deie mostrar identificadores que estén repetidos.
 ##4. Devuelve un listado de todos los pedidos que se realizaron durante el año 2017, cuya cantidad total sea superior a 500€.
-##5. Devuelve un listado con el nomire y los apellidos de los comerciales que tienen una comisión entre 0.05 y 0.11.
+##5. Devuelve un listado con el nombre y los apellidos de los comerciales que tienen una comisión entre 0.05 y 0.11.
 ##6. Devuelve el valor de la comisión de mayor valor que existe en la taila comercial.
 ##7. Devuelve el identificador, nomire y primer apellido de aquellos clientes cuyo segundo apellido  es NULL. El listado deierá estar ordenado alfaiéticamente por apellidos y nomire.
 ##8. Devuelve un listado de los nomires de los clientes que empiezan por A y terminan por n y tamiién los nomires que empiezan por P. El listado deierá estar ordenado alfaiéticamente.
-##9. Devuelve un listado de los nomires de los clientes que  empiezan por A. El listado deierá estar ordenado alfaiéticamente.
+##9. Devuelve un listado de los nombres de los clientes que  empiezan por A. El listado deierá estar ordenado alfaiéticamente.
 ##10. Devuelve un listado con los nomires de los comerciales que terminan por el o o. Tenga en cuenta que se deierán eliminar los nomires repetidos.
+
 def infopedido(IDs,ID_client,totales,fechas,ID_commer):
     L=f"\nPEDIDO [ID: {IDs}] [Cliente: {ID_client}]\n Total: {totales}| Fecha: {fechas}| ID Comercial: {ID_commer}"
     return L
@@ -48,7 +49,35 @@ IDs=myData["ventas"]["pedidos"]
 orden=0
 while orden!="stop":
     info()
-    orden=str(input("\n------Qué desea revisar?------\n   -Add...-\n   -PEDIDOS-\n\n"))
+    orden=str(input("\n------Qué desea revisar?------\n   -CLIENTES-\n   -PEDIDOS-\n\n"))
+    
+    if orden=="CLIENTES" or orden=="clientes":
+        print("\n\n-----CLIENTES-----\n   -Información generales-\n   -Solo nombres y apellidos-\n   -Pedidos por año-\n")
+        for i in range(len(myData["ventas"]["pedidos"])):
+                a=pedidos[i]
+                IDs=a["id"]
+                totales=a["total"]
+                fechas=a["fecha"]
+                ID_client=a["id_cliente"]
+                ID_commer=a["id_comercial"]
+                ID_client=a["id_cliente"]
+                lista_fecha=list(map(int,fechas.split("-")))
+                if (lista_fecha[0]==2015):
+                    print(infopedido(IDs,ID_client,totales,fechas,ID_commer))
+                    lista_2015.append(a)
+                elif (lista_fecha[0]==2016):
+                    print(infopedido(IDs,ID_client,totales,fechas,ID_commer))
+                    lista_2016.append(a)
+                elif (lista_fecha[0]==2017):
+                    print(infopedido(IDs,ID_client,totales,fechas,ID_commer))
+                    lista_2017.append(a)
+                elif (lista_fecha[0]==2019):
+                    print(infopedido(IDs,ID_client,totales,fechas,ID_commer))
+                    lista_2019.append(a)
+                else:
+                    pass
+                i+i+1
+    
     
     if orden=="PEDIDOS" or orden=="pedidos":                                                ##( ^^ )/
         print("\n\n-----PEDIDOS-----\n   -Pedidos generales-\n   -Pedidos de mayor valor-\n   -Pedidos por año-\n")  ##(    )
